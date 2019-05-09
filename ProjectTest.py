113362f44f7a9094c22aae7c42bec6c34bcfd38d
Arsham Atighechi <arshama@kth.se>
2019-05-09 16:19:30 +0200

    test filer

---
+++ ProjectTest.py
@@ -0,0 +1,11 @@
+# Just disables the warning, doesn't enable AVX/FMA
+import os
+import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
