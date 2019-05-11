3fd5ea6d5efebeb4f673825732bff7d735979788
DkuzZ <daniel.atighechi@gmail.com>
2019-05-11 17:34:46 +0200

    One working Conv2D layer

--- testBuildTfKeras.py
+++ testBuildTfKeras.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+#from tensorflow import Conv2D
 # import numpy.plot as plt
 
 
@@ -8,14 +9,21 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
+x_train = x_train.astype('float32')/255
+x_test = x_test.astype('float32')/255
+
+print(len(y_train[0]))
+
 
 #z-score
-mean = np.mean(x_train,axis=(0,1,2,3))
-std = np.std(x_train,axis=(0,1,2,3))
-x_train = (x_train-mean)/(std+1e-7)
-x_test = (x_test-mean)/(std+1e-7)
+#mean = np.mean(x_train,axis=(0,1,2,3))
+#std = np.std(x_train,axis=(0,1,2,3))
+#x_train = (x_train-mean)/(std+1e-7)
+#x_test = (x_test-mean)/(std+1e-7)
+
+num_classes = 10
+#y_train = np_utils.to_categorical(y_train,num_classes)
+#y_test = np_utils.to_categorical(y_test,num_classes)
 
 # # print(x_train)
 
@@ -33,19 +41,27 @@ x_test = (x_test-mean)/(std+1e-7)
 # (x_train, y_train),(x_test, y_test) = mnist.load_data()
 # x_train, x_test = x_train / 255.0, x_test / 255.0
 
-model = tf.keras.models.Sequential([
-  # tf.keras.layers.Conv2D(filters = 32, kernel_size = 1, padding = "same"),
-  tf.keras.layers.Flatten(input_shape=(32, 32, 3)),
-  tf.keras.layers.Dense(512, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+#model = tf.keras.models.Sequential([
+ #  tf.keras.layers.Conv2D(filters = 32,kernel_size = 2, padding='same', activation ='relu',input_shape=(32,32,3),  #[1:]),
 
-# model.add(Convolution2D(64, 3, 3, border_mode = 'same', input_shape = (32, 32, 3)))
-# model.add(Flatten())
+  #tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  #tf.keras.layers.Dropout(0.2),
+  #tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+  
+  
+#  tf.keras.layers.Flatten(input_shape=(32, 32, 3))
+#])
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(filters =128, kernel_size =3, padding = "same", activation = 'relu', input_shape=x_train.shape[1:]))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
+model.add(tf.keras.layers.Dropout(0.2))
+
+ #model.add(Convolution2D(64, 3, 3, border_mode = 'same', input_shape = (32, 32, 3)))
+model.add(tf.keras.layers.Flatten())
 model.compile(optimizer='SGD',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+             loss='sparse_categorical_crossentropy',
+             metrics=['accuracy'])
+			 
 
 model.fit(x_train, y_train, epochs=5)
 model.evaluate(x_test, y_test)
