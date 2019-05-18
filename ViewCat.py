8f82922515bccfee88b197bba5ea7b3fd02a5840
heho07 <herman.hogman.ording@gmail.com>
2019-05-18 17:40:54 +0200

    made it so we can save layer visualization

--- ViewCat.py
+++ ViewCat.py
@@ -1,22 +1,24 @@
+# Visualizes the layer output from a pre-trained model
+
+# Takes a picture of a truck from the training data set from CIFAR-10
+# Loads model
+# Creates images from layer output from a given layer
+
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras import backend as K
-#from tensorflow import Conv2D
-# import numpy.plot as plt
 import csv
-
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
-
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-x_train = x_train.astype('float32')/255  # .astype('float32')
+x_train = x_train.astype('float32')/255 
 x_test = x_test.astype('float32')/255
 
-
+# image of a truck
 truck_image = x_train[1,:]
 truck_image = np.asarray(truck_image)
 
@@ -26,7 +28,7 @@ model = tf.keras.models.load_model(loading_checkpoint_path)
 model.evaluate(x_test, y_test)  #checking that it works
 
 
-# get the layer output
+# get the layer output from a certain layer
 def get_nth_layer_output(layer, image):
     image = np.expand_dims(image,axis=0)
     get_layer_output = K.function([model.layers[0].input],
@@ -36,7 +38,7 @@ def get_nth_layer_output(layer, image):
     return layer_output
 
 # visualize the layer output
-def visualize_layer_output(layer_output):
+def visualize_layer_output(layer_output, model_name, show = True):
     height, width, depth = layer_output.shape
     nb_plot = int(np.rint(np.sqrt(depth)))
     fig = plt.figure(figsize=(20, 20))
@@ -44,7 +46,15 @@ def visualize_layer_output(layer_output):
         plt.subplot(nb_plot, nb_plot, i+1)
         plt.imshow(layer_output[:,:,i], cmap='gray')
         plt.title('l: {}'.format(i+1))
-    plt.show()
+    plt.savefig('./images/layer_visualization/' + model_name + '_layer_' + str(layer))
+    if show:
+        plt.show()
+        
 
-truck_output_layer_6 = get_nth_layer_output(6, truck_image)
-visualize_layer_output(truck_output_layer_6)
+def get_and_visualize_layer_output(layer, image, model_name = "foo", show = True):
+    truck_output_layer_n = get_nth_layer_output(layer, image)
+    visualize_layer_output(truck_output_layer_n, model_name, show)
+
+
+get_and_visualize_layer_output(6, truck_image, "replicating_study_learning_rate0.01", True)
+
